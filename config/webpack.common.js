7d619f9cf37406f3d5b3a7af7ebff0e84333e788
Kharoh <Gaulu03@gmail.com>
2021-07-24 22:48:58 +0200

    created default webpack and react folder architecture

---
+++ config/webpack.common.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const globImporter = require('node-sass-glob-importer');
+
+module.exports = {
+  entry: './src/frontend/index.tsx',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                importer: globImporter()
+              }
+            }
+          }
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          'svg-url-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader'
+        ],
+      },
+    ],
+  },
+  output: {
+    publicPath: '/',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '..', 'dist'),
+  },
+};
